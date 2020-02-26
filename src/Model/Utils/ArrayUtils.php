e201af58210750cb8845de192de7bac36becc896
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2020-02-26 11:20:49 +0100

    array utils

--- src/Model/Utils/ArrayUtils.php
+++ src/Model/Utils/ArrayUtils.php
@@ -11,21 +11,24 @@ class ArrayUtils {
      * @param string|array $fieldName
      * @return array
      */
-    public static function replaceKeys($items, $fieldID, $fieldName){
+    public static function replaceKeys($items, $fieldID, $fieldName = null){
         foreach($items as &$item){
             $item['id'] = $item[$fieldID];
-            if( is_array($fieldName) ){
-                $item['name'] = '';
-                for($i=0; $i<count($fieldName); $i++){
-                    $key = $fieldName[$i];
-                    $value = $item[$key];
-                    if( $i != 0 ) $value = '(' . $value . ')';
-                    $item['name'] .= ' '.$value;
-                    unset($item[$key]);
+            if( $fieldName !== null ) {
+                if (is_array($fieldName)) {
+                    $name = '';
+                    for ($i = 0; $i < count($fieldName); $i++) {
+                        $key = $fieldName[$i];
+                        $value = $item[$key];
+                        if ($i != 0) $value = ' (' . $value . ')';
+                        $name .= ' ' . $value;
+                        if( $key != 'name' ) unset($item[$key]);
+                    }
+                    $item['name'] = $name;
+                } else {
+                    $item['name'] = $item[$fieldName];
+                    unset($item[$fieldName]);
                 }
-            }else{
-                $item['name'] = $item[$fieldName];
-                unset($item[$fieldName]);
             }
             unset($item[$fieldID]);
         }
