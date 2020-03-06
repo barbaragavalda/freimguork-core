2ab1438b0a03da0ac8c3b60e25828ca129e32d7e
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2020-03-06 13:22:25 +0100

    configure allow origins on json responses

--- src/View/Response/Json.php
+++ src/View/Response/Json.php
@@ -2,6 +2,9 @@
 
 namespace Core\View\Response;
 
+use Core\Model\Utils\StringUtils;
+use Core\Utils\Config;
+
 /**
  * Class Json
  *
@@ -17,6 +20,24 @@ class Json extends Response {
      * set the header of the response
      */
     public function __construct(){
+        // add allow origin header
+        $config = Config::getInstance();
+        $allowOrigins = $config->get('api', 'allow-origin');
+        if( count($allowOrigins) && array_key_exists('HTTP_ORIGIN', $_SERVER) ){
+            $origin = $_SERVER['HTTP_ORIGIN'];
+            if( !StringUtils::endsWidth($origin, '/') ) $origin .= '/';
+
+            foreach($allowOrigins as $allowOrigin){
+                $compareOrigin = $allowOrigin;
+                if( !StringUtils::endsWidth($compareOrigin, '/') ) $compareOrigin .= '/';
+
+                if( $compareOrigin == $origin ){
+                    $this->setHeader('Access-Control-Allow-Origin', $allowOrigin);
+                    break;
+                }
+            }
+        }
+
         $this->setHeaderType('application/json');
     }
 
