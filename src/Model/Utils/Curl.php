5f2e2c1e52517c6fbe7e96c4713fa28f8dd44654
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2020-06-08 10:08:56 +0200

    improvements on curl function

--- src/Model/Utils/Curl.php
+++ src/Model/Utils/Curl.php
@@ -4,13 +4,26 @@ namespace Core\Model\Utils;
 
 class Curl {
 
-    public static function get($url, $header = null){
+    /**
+     * do curl request
+     * @param string $url       request URL
+     * @param array $params     request params
+     * @param array $header     custom headers
+     * @param string $path      log file
+     * @return array|mixed
+     */
+    public static function get($url, $params = array(), $header = null, $path = null){
         $curl = curl_init();
 
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        if( count($params) ){
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
+
         if( $header != null ){
             curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         }
@@ -18,6 +31,9 @@ class Curl {
         $result = array();
         $output = curl_exec($curl);
         if( !curl_errno($curl) ){
+            if( $path != null ){
+                file_put_contents($path, $output);
+            }
             $result = json_decode($output, true);
         }
 
