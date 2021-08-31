f811facefa02ff7b8b5c9f3c520a8b6db344eabc
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2021-08-31 13:36:35 +0200

    push notifications with image

--- src/Model/Push/Push.php
+++ src/Model/Push/Push.php
@@ -9,8 +9,9 @@ use Core\Model\Model;
 use Core\Model\Utils\Mail;
 use Core\Utils\Config;
 
-class Push extends Model {
-    
+class Push extends Model
+{
+
     /**
      * @var string  app name
      */
@@ -36,92 +37,108 @@ class Push extends Model {
      */
     private $reportEmail = '';
 
-    public function __construct($doLog = false){
+    public function __construct($doLog = false)
+    {
         parent::__construct();
 
         $this->doLog = $doLog;
 
         // url scheme
-        $config = Config::getInstance();
-        $webserviceConfig = $config->get('webservice');
-        $this->appName = $webserviceConfig['app_name'];
-        $this->urlScheme = $webserviceConfig['url_scheme'];
+        $config            = Config::getInstance();
+        $webserviceConfig  = $config->get('webservice');
+        $this->appName     = $webserviceConfig['app_name'];
+        $this->urlScheme   = $webserviceConfig['url_scheme'];
         $this->reportEmail = $config->get('push', 'report');
-        
+
         $this->hasStatistics = $this->mysql->tableExists('appacman_push_statistic');
     }
 
     /**
      * send push notification
-     * @param $platforms
-     * @param $message
+     *
+     * @param       $platforms
+     * @param       $message
      * @param array $notification
-     * @param int $pushID
+     * @param int   $pushID
      */
-    public function send($platforms, $message, $notification, $pushID = null){
+    public function send($platforms, $message, $notification, $pushID = null)
+    {
         $deepLink = $notification['deeplink'];
-        if( $pushID && $this->hasStatistics ){
+        if ($pushID && $this->hasStatistics) {
             $this->id = $pushID;
-            if( $deepLink && strpos($deepLink, '?') !== false ){
+        }
+
+        if (array_key_exists('id', $notification)) {
+            $pushID = $notification['id'];
+        }
+        if ($pushID) {
+            if ($deepLink && strpos($deepLink, '?') !== false) {
                 $deepLink .= '&';
-            }else{
+            } else {
                 $deepLink .= '?';
             }
-            $deepLink .= 'push_id=' . $this->id;
+            $deepLink .= 'pushID=' . $pushID;
         }
 
         $urlScheme = '';
-        if( $deepLink != '' ) $urlScheme = $this->urlScheme . $deepLink;
+        if ($deepLink != '') {
+            $urlScheme = $this->urlScheme . $deepLink;
+        }
 
-        $image = '';
+        $image   = '';
         $imageID = array_key_exists('image', $notification) ? $notification['image'] : '';
-        if($imageID){
-            $file = new File($imageID);
+        if ($imageID) {
+            $file  = new File($imageID);
             $image = $file->getAbsolutePath();
         }
 
         $android = $ios = array();
-        foreach($platforms as $platform){
+        foreach ($platforms as $platform) {
             $tokens = explode(',', $platform['tokens']);
-            switch( $platform['name'] ){
-                case 'android': $android = $tokens; break;
-                case 'ios':     $ios = $tokens;     break;
+            switch ($platform['name']) {
+                case 'android':
+                    $android = $tokens;
+                    break;
+                case 'ios':
+                    $ios = $tokens;
+                    break;
             }
         }
-        
-        $devices = 0;
+
+        $devices      = 0;
         $emailMessage = '<p><b>Texto: </b>' . $message . '</p>';
-        if( count($android) ){
-            if( IS_DEV ) {
+        if (count($android)) {
+            if (IS_DEV) {
                 echo '<p>Fake push notification for Android: <b>' . $message . '</b></p>';
-            }else{
+            } else {
                 $pushAndroid = new Android($message, $android, $urlScheme, $image, $this->doLog);
                 $pushAndroid->send();
                 $pushAndroid->close();
 
                 $emailMessage .= '<p><b>Android: </b>' . $pushAndroid->getEmailResult() . '</p>';
-                $devices += $pushAndroid->getOk();
+                $devices      += $pushAndroid->getOk();
             }
         }
 
-        if( count($ios) ){
-            if( IS_DEV ){
-                echo '<p>Fake push notification for iOS: <b>'.$message.'</b></p>';
-            }else{
+        if (count($ios)) {
+            if (IS_DEV) {
+                echo '<p>Fake push notification for iOS: <b>' . $message . '</b></p>';
+            } else {
                 $pushiOS = new iOSPush($message, $ios, $urlScheme, $image, $this->doLog);
                 $pushiOS->send();
                 $pushiOS->close();
 
                 $emailMessage .= '<p><b>iOS: </b>' . $pushiOS->getEmailResult() . '</p>';
-                $devices += $pushiOS->getOk();
+                $devices      += $pushiOS->getOk();
             }
         }
 
         $this->statistics($message, $emailMessage, $devices);
     }
-    
-    private function statistics($message, $emailMessage, $devices){
-        if( $message && $emailMessage && $this->doLog ){
+
+    private function statistics($message, $emailMessage, $devices)
+    {
+        if ($message && $emailMessage && $this->doLog) {
             $admin = new Admin();
             $email = new Mail();
             $email->send(
@@ -131,8 +148,8 @@ class Push extends Model {
                 $emailMessage
             );
         }
-        
-        if( $this->hasStatistics ){
+
+        if ($this->hasStatistics) {
             $statistics = new Statistic($this->id);
             $statistics->update($devices);
         }
