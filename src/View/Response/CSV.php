6c1d00fe681eed81a4b6c985d7943a3da7faf527
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2021-10-22 09:26:55 +0200

    save CSV to destination not browser

--- src/View/Response/CSV.php
+++ src/View/Response/CSV.php
@@ -1,65 +1,81 @@
 <?php
-    
+
 namespace Core\View\Response;
 
 /**
  * Class CSV
  * @package Core\Views\Response
  */
-class CSV extends Response{
-    
+class CSV extends Response
+{
+
     /**
      * tableName
      * @var string
      */
     private $tableName = '';
-    
-    public function __construct($tableName){
+
+    public function __construct($tableName)
+    {
         $this->tableName = $tableName;
     }
-    
+
     /**
      * creates a file and downloads it through the browser
-     * @param array $info   Is always null
+     *
+     * @param array  $info info to write to the file
+     * @param string $path path to file
+     *
+     * @return string
      */
-    public function initResponse( $info = null ) {
+    public function initResponse($info = null, $path = null)
+    {
         $file = $this->tableName . '-' . date('Y-m-d-His') . '.csv';
-        if( array_key_exists('export_without_date', $info) ){
+        if (array_key_exists('export_without_date', $info)) {
             $file = $this->tableName . '.csv';
         }
 
-        $this->setHeaderType('application/csv');
-        $this->setHeader('Content-Disposition', 'attachment; filename="' . $file . '";');
-
-        $f = fopen('php://output', 'w');
+        $destination = $path;
+        if ($path == null) {
+            $destination = 'php://output';
+            $this->setHeaderType('application/csv');
+            $this->setHeader('Content-Disposition', 'attachment; filename="' . $file . '";');
+        } else {
+            $destination .= $file;
+        }
+        $f = fopen($destination, 'w');
         fputs($f, $bom = chr(0xEF) . chr(0xBB) . chr(0xBF));
-        
+
         // titles
         array_unshift($info['csv']['titles'], '');
         fputcsv($f, $info['csv']['titles'], ';');
-        
+
         // list
-        foreach($info['csv']['list'] as $item){
-            foreach($item as &$value){
+        foreach ($info['csv']['list'] as $item) {
+            foreach ($item as &$value) {
                 $value = strip_tags($value);
             }
             fputcsv($f, $item, ';');
         }
+
+        return $destination;
     }
 
     /**
      * generate excel
+     *
      * @param string $fileName
-     * @param array $content
+     * @param array  $content
      */
-    public static function createCSV($fileName, $content){
+    public static function createCSV($fileName, $content)
+    {
         $file = $fileName . '.csv';
         header('Content-Type: application/csv');
         header('Content-Disposition: attachment; filename="' . $file . '";');
         $f = fopen('php://output', 'w');
         fputs($f, $bom = chr(0xEF) . chr(0xBB) . chr(0xBF));
 
-        foreach($content as $line){
+        foreach ($content as $line) {
             fputcsv($f, $line, ';');
         }
     }
