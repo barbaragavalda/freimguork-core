c765934514d57065267e69a21c51140f6daa2d68
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2022-05-28 10:22:05 +0200

    string util str_pad

--- src/Model/Utils/StringUtils.php
+++ src/Model/Utils/StringUtils.php
@@ -411,5 +411,11 @@ class StringUtils {
        return mb_strtoupper($acronym);
    }
 
+    public static function mb_str_pad( $input, $pad_length, $pad_string = ' ', $pad_type = STR_PAD_RIGHT)
+    {
+        $diff = strlen( $input ) - mb_strlen( $input );
+        return str_pad( $input, $pad_length + $diff, $pad_string, $pad_type );
+    }
+
 
 }
